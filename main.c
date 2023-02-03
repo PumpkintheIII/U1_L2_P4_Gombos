673645d36627555b45f100787e83224c9b412c9a
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-03 13:12:35 +0000

    Iteration 2

--- main.c
+++ main.c
@@ -12,12 +12,12 @@ int main(void) { //define main function
   printf("Please enter how many apples you would like to buy: ");
   //ask how many apples the user would like to buy
   scanf("%d", &appleNum); //accepts user input, formats as integer
-  printf("Please enter how much does each apple costs: ");
+  printf("Please enter how much does each apple costs: $");
   //ask how much each apple costs
   scanf("%f", &applePrice); //accepts user input, formats as float
 
   applePrice *= appleNum; //finds total price of apples
-  printf("The cost of the apples is: %f", applePrice); 
+  printf("The cost of the apples is: $%f", applePrice); 
   //outputs price of apples
   
   return 0;
